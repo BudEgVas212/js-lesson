deff0395be93285e76bbac93bccc8d7551b56dcb
BudEgVas212 <vaso_yashik@mail.ru>
2022-12-29 18:51:10 +0300

    step_4

--- script.js
+++ script.js
@@ -73,3 +73,85 @@ catch(a) {
     console.log("Отрицательный результат функции")
 }
 finally{console.log("Функция проверена")}
+
+
+
+const c = "I am the best boy"
+function magic (a){
+    return a.split(" ")
+}
+console.log(magic(c))
+
+
+const sr = "I am the best girl"
+let m = 10
+function podstroka (st,o){
+    return st.substring(0, o)
+}
+console.log(podstroka(sr, m))
+
+
+const stroka = "I love my dog"
+function pupamupa (stroka){
+    let mass = stroka.split(" ")
+    let masc = mass.join("-")
+    return masc.toUpperCase()
+}
+console.log(pupamupa(stroka))
+
+const leg = "нога"
+function firstWord (leg){
+    return leg[0].toUpperCase()+leg.slice(1) 
+}
+
+console.log(firstWord(leg))
+
+
+const stroca = "12"
+let n = 5
+function numbers (stroca, n){
+    let m = n-stroca.length
+    let massiv = stroca.split()
+    for(let i = 0;i < m;i++){
+        massiv.push("0")
+    }
+    return massiv.join()
+}
+console.log(numbers(stroca, n))
+
+
+const SR = "nbgtyuikgoi;u;op"
+const sr = "fgpkdjfogifjn"
+
+function sravnenie (SR, sr){
+    return SR.localeCompare(sr)
+}
+console.log(sravnenie(SR, sr))
+
+const s = "2022-12-31"
+function formdate (data){
+   let d = data.split("-") 
+   let a = d[2]+"."+d[1]+"."+d[0]
+   
+   return a
+}
+console.log(formdate(s))
+
+const l = "10 + 5"
+function math (strochka){
+    let s = strochka.split()
+    if(s[1]== "+"){
+        return Number(s[0]) + Number(s[2])
+    }
+    if(s[1] == "-"){
+        return Number(s[0]) - Number(s[2])
+    }
+    if(s[1] == "/"){
+        return Number(s[0]) / Number(s[2])
+    }
+    if(s[1] == "*"){
+        return Number(s[0]) * Number(s[2])
+    }
+}
+console.log(math(l))
+
